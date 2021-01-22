3ecc043979e619dbf00c634b4f9d7384725015ff
Andrés <andres.gerz@gmail.com>
2021-01-22 10:38:59 -0300

    I added PHP 7.4.

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+  echo "<h1>" . "PHP" . " Bootcamp" . "</h1>";
+  echo "<br><br>";
+
+  $number = 42;
+  $string = "something";
+  $boolean = true;
+  $array = ["name" => "Andy", "age" => 42];
+  
+
+  echo $number . " " . var_dump($number); 
+  echo "<br><br>";
+  echo $string . " " . var_dump($string);
+  echo "<br><br>";
+  echo $boolean . " " . var_dump($boolean);  
+  echo "<br><br>";
+  echo $array . " " . var_dump($array); 
+
+
+  
